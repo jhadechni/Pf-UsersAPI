a2f3342e0a94af884a2e6038fb2ec2521633227f
Jaime Sierra <49314853+jhadechni@users.noreply.github.com>
2022-05-17 18:44:51 -0500

    fixing model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
   name: {type : String, required: true},
   surnames: {type: String, required: true},
   username: {type: String, required: true, unique: true},
-  password: {type: String, required: true, unique: true},
-  cedula: {type: String, required: true},
+  password: {type: String, required: true},
+  cedula: {type: String, required: true, unique: true},
   email: {type: String, required: true},
   blockchain_PK: { 
     type: String, 

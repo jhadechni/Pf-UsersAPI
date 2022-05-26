2331c470cdc53cc2e76c72f72fd6a7c96ea24a9d
Jaime Sierra <49314853+jhadechni@users.noreply.github.com>
2022-05-26 16:21:38 -0500

    fixing pdf creation

--- src/config/filesCreation.js
+++ src/config/filesCreation.js
@@ -1,45 +1,58 @@
 const controller = {}
 const fs = require("fs").promises;
-const pdf = require('html-pdf-node');
+const pdf = require('html-pdf');
 
-controller.createPDFTIL = async (transactions) =>  {
+controller.createPDFTIL = async (transactions) => {
     try {
-    //fs.unlinkSync('src/outputs/salida.pdf')
- 
-    let contenidoHtml = await fs.readFile('src/templates/templateTIL.html', 'utf8')
-    let template = `
+        //fs.unlinkSync('src/outputs/salida.pdf')
+
+        let contenidoHtml = await fs.readFile('src/templates/templateTIL.html', 'utf8')
+        let template = `
             <hr>
             <p style="text-align: left;"><strong>Descripcion:</strong></p>
             <p style="text-align: left;"><strong>DE:</strong> {{prevOwner}}<br /><strong>A:</strong> {{actualOwner}}</p>
             <p style="text-align: left;">{{description}}
             <p style="text-align: left;">Cedula admin: {{adminID}}
             </p>`;
-    let contenido = ``
-    transactions.forEach((element) => {
-        newData = template.replace("{{prevOwner}}", element.prevOwner)
-                          .replace("{{actualOwner}}", element.actualOwner)
-                          .replace("{{description}}", element.description)
-                          .replace("{{adminID}}", element.adminId)
-                          .replace("null", "X")
-        contenido += newData
-    })
-    
-    contenidoHtml = contenidoHtml.replace("{{descriptionALL}}", contenido)
-                                 .replace("{{city}}", transactions[0].city)
-                                 .replace("{{enrollmentNumber}}", transactions[0].enrollmentNumber)
-                                 .replace("{{status}}", transactions[transactions.length - 1].status)
-    console.log(contenidoHtml)
-    const file = { content: contenidoHtml };
-    const options = { format: 'A4' };
-    const pdfBuffer = await pdf.generatePdf(file,options)
-    console.log(pdfBuffer)
+        let contenido = ``
+        transactions.forEach((element) => {
+            newData = template.replace("{{prevOwner}}", element.prevOwner)
+                .replace("{{actualOwner}}", element.actualOwner)
+                .replace("{{description}}", element.description)
+                .replace("{{adminID}}", element.adminId)
+                .replace("null", "X")
+            contenido += newData
+        })
 
-    return pdfBuffer 
-        
+        contenidoHtml = contenidoHtml.replace("{{descriptionALL}}", contenido)
+            .replace("{{city}}", transactions[0].city)
+            .replace("{{enrollmentNumber}}", transactions[0].enrollmentNumber)
+            .replace("{{status}}", transactions[transactions.length - 1].status)
+        console.log(contenidoHtml)
+        const options = { format: 'A4' };
+        const pdfBuffer = await createPDF(contenidoHtml,options)
+       
+        console.log(pdfBuffer)
+
+
+        return pdfBuffer
+
     } catch (error) {
         console.log(error)
     }
-    
+
+}
+
+function createPDF(html,options) {
+    return new Promise((resolve, reject) => {
+        pdf.create(html,options).toBuffer(function (err, buffer) {
+            if (err) {
+                reject(err)
+            }
+            resolve(buffer);
+        });
+    }
+    )
 }
 
 
